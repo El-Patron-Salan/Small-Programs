0d3f46706ec5a9d53b4bbec34629cb7923a4c71d
alan <salan2000@wp.pl>
2020-11-15 14:10:14 +0100

    - make main program that use encryption and decryption classes

---
+++ src/challenges/encryption_decryption/MainProgram.java
@@ -0,0 +1,44 @@
+package challenges.encryption_decryption;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainProgram {
+
+    private static void menu(){
+
+        System.out.println("\t1 - encryption" +
+                           "\n\t2 - decryption");
+        System.out.print("Choose option: ");
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            Scanner input = new Scanner(System.in);
+            menu ();
+            int option = input.nextInt();
+
+            switch (option){
+                case 1:
+                    System.out.println("Message to encrypt: ");
+                    String toEncrypt = input.nextLine();
+                    System.out.print("Offset: ");
+                    int offset = input.nextInt();
+                    System.out.println("Encrypted message: " + Encryption.encrypt(toEncrypt, offset));
+                    break;
+                case 2:
+                    System.out.println("Message to decrypt: ");
+                    String toDecrypt = input.nextLine();
+                    System.out.print("Offset: ");
+                    int decryptOffset = input.nextInt();
+                    System.out.println("Decrypted message: " + Decryption.decrypt(toDecrypt, decryptOffset));
+                    break;
+                default:
+                    System.out.println("There isn't such an option");
+            }
+        }catch(InputMismatchException e){
+            System.out.println(e + " - wrong input");
+        }
+
+    }
+}
