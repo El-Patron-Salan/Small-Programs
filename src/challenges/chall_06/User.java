511ed75ba51851c27b403105c145a965c0753924
alan <salan2000@wp.pl>
2020-12-26 12:01:15 +0100

    Added input section with choice of player choosing

--- src/challenges/chall_06/User.java
+++ src/challenges/chall_06/User.java
@@ -1,6 +1,8 @@
 package challenges.chall_06;
+import java.util.Scanner;
 
 public class User {
+    Scanner input = new Scanner(System.in);
     private String name;
 
     public String getName(){
@@ -8,6 +10,34 @@ public class User {
     }
     public void setName(String name){
         this.name = name;
+    }
+    public void askName(){
+        System.out.println("What's your name?");
+        System.out.print("Input: ");
+        name = input.next();
+    }
+    public int userChoice(){
+        System.out.println("You can choose:\nROCK | PAPER | SCISSORS | LIZARD | SPOCK");
+        String choice = input.next();
+        choice = choice.toUpperCase();
+        String s = choice.substring(0, 2);
+
+        switch (s) {
+            case "RO":
+                return Rules.ROCK;
+            case "PA":
+                return Rules.PAPER;
+            case "SC":
+                return Rules.SCISSORS;
+            case "LI":
+                return Rules.LIZARD;
+            case "SP":
+                return Rules.SPOCK;
+            default:
+                userChoice();
+                return 0;
+        }
+
     }
 
 }
