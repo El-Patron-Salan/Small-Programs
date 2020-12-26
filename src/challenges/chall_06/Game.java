e984e64eaf2b5835bf45337b5c6323ef4da0b02f
alan <salan2000@wp.pl>
2020-12-26 13:19:13 +0100

    Completed program

--- src/challenges/chall_06/Game.java
+++ src/challenges/chall_06/Game.java
@@ -11,6 +11,12 @@ public class Game {
 
     public Game(){}
 
+    public static void main(String[] args){
+        Game g = new Game();
+        g.askUserName();
+        g.game();
+    }
+
     public void game(){
         //takes input
         int playerInput = user.userChoice();
@@ -23,20 +29,43 @@ public class Game {
         switch (compare){
             case 0:
                 System.out.println("It's a TIE!");
+                break;
             case 1:
                 System.out.println("Player WINS");
                 userScore++;
+                break;
             case -1:
                 System.out.println("Computer WINS");
                 computerScore++;
+                break;
         }
         gamesCounter++;
 
+        if(userScore == 5){
+            System.out.println("You've won the game!");
+            new Game();
+        }
+        if(computerScore == 5){
+            System.out.println("Computer's beaten you!");
+            new Game();
+        }
+        //another game
+        if(user.playAgain() == 'Y') {
+            System.out.println();
+            game();
+        }
+        else
+            printStatistics();
     }
     public void askUserName(){
         user.askName();
     }
     public String userName(){
         return user.getName();
+    }
+    public void printStatistics(){
+        System.out.println("Number of games: " + gamesCounter);
+        System.out.println(userName() + " score:" + userScore);
+        System.out.println("Computer score: " + computerScore);
     }
 }
