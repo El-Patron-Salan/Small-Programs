12c7765ee223e20df781e1f861a80f36be9cf713
alan <salan2000@wp.pl>
2020-12-27 22:33:22 +0100

    [1]Euler Problem

---
+++ src/challenges/chall_07/EulerFirstProblem.java
@@ -0,0 +1,23 @@
+package challenges.chall_07;
+
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class EulerFirstProblem {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i = 1; i < 1000; i++){
+            if((i % 3 == 0) || (i % 5 == 0)) list.add(i);
+        }
+//        Iterator<Integer> it = list.iterator();
+//        while(it.hasNext()){
+//            System.out.println(it.next());
+//        }
+        int sum = 0;
+        for(int i: list){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
