739dc04121d1a6e6df87b063e59971748595c9d9
alan <salan2000@wp.pl>
2021-01-08 23:59:17 +0100

    Added method to replace word with [_]

--- src/challenges/chall_08/Driver.java
+++ src/challenges/chall_08/Driver.java
@@ -1,8 +1,10 @@
 package challenges.chall_08;
-
+import java.io.FileNotFoundException;
 public class Driver {
 
-
-
+    private static String hideWord() throws FileNotFoundException{
+        String hide;
+        return hide = new String(new char[GenerateWord.getRandomWord().length()]).replace("\0", "_");
+    }
 
 }
