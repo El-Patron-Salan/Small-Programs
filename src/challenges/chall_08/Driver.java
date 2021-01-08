7aa6fc552b34544c39cfa97c179633c6a57f8b29
alan <salan2000@wp.pl>
2021-01-09 00:38:16 +0100

    Shortened the method which hides the word

--- src/challenges/chall_08/Driver.java
+++ src/challenges/chall_08/Driver.java
@@ -3,8 +3,7 @@ import java.io.FileNotFoundException;
 public class Driver {
 
     private static String hideWord() throws FileNotFoundException{
-        String hide;
-        return hide = new String(new char[GenerateWord.getRandomWord().length()]).replace("\0", "_");
+        return new String(new char[GenerateWord.getRandomWord().length()]).replace("\0", "_");
     }
 
 }
