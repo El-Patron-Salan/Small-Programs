c396873f6d7daf31cd8f12cb9b107bef22989818
alan <salan2000@wp.pl>
2021-01-09 21:57:44 +0100

    Added method to handle main game mechanics

--- src/challenges/chall_08/Driver.java
+++ src/challenges/chall_08/Driver.java
@@ -2,8 +2,13 @@ package challenges.chall_08;
 import java.io.FileNotFoundException;
 public class Driver {
 
-    private static String hideWord() throws FileNotFoundException{
-        return new String(new char[GenerateWord.getRandomWord().length()]).replace("\0", "_");
+    private static String hideWord(String hideRandWord) throws FileNotFoundException{
+        return new String(new char[hideRandWord.length()]).replace("\0", "_");
     }
+
+    public static void game(String guess){
+
+    }
+
 
 }
