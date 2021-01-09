640ae9b471664ff634236e5468a245e868bea4f2
alan <salan2000@wp.pl>
2021-01-09 23:59:35 +0100

    Added main logic of game

--- src/challenges/chall_08/Driver.java
+++ src/challenges/chall_08/Driver.java
@@ -6,7 +6,19 @@ public class Driver {
         return new String(new char[hideRandWord.length()]).replace("\0", "_");
     }
 
-    public static void game(String guess){
+    public static void game(String guess) throws FileNotFoundException{
+        String importWord = GenerateWord.getRandomWord();
+        String newHide = "";
+        for(int i = 0; i < importWord.length(); ++i){
+            if(importWord.charAt(i) == guess.charAt(0))
+                newHide += guess.charAt(0);
+
+            else if(hideWord(importWord).charAt(i) != '_')
+                newHide += importWord.charAt(i);
+            else
+                newHide += "_";
+
+        }
 
     }
 
