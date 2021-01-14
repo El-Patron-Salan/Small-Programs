e88a35582ab83811b5ab3baeff40380ab2e24e5b
alan <salan2000@wp.pl>
2021-01-14 23:56:03 +0100

    Cleared off outputs with "Try again"

--- src/challenges/chall_08/AnimationOfHangman.java
+++ src/challenges/chall_08/AnimationOfHangman.java
@@ -4,7 +4,6 @@ public class AnimationOfHangman {
 
     public static void hangmanImage(int lives) {
         if (lives == 6) {
-            System.out.println("Wrong guess, try again");
             System.out.println();
             System.out.println();
             System.out.println();
@@ -13,7 +12,6 @@ public class AnimationOfHangman {
             System.out.println();
         }
         if (lives == 5) {
-            System.out.println("Wrong guess, try again");
             System.out.println("   |");
             System.out.println("   |");
             System.out.println("   |");
@@ -24,7 +22,6 @@ public class AnimationOfHangman {
             System.out.println("___|___");
         }
         if (lives == 4) {
-            System.out.println("Wrong guess, try again");
             System.out.println("   ____________");
             System.out.println("   |");
             System.out.println("   |");
@@ -36,7 +33,6 @@ public class AnimationOfHangman {
             System.out.println("___|___");
         }
         if (lives == 3) {
-            System.out.println("Wrong guess, try again");
             System.out.println("   ____________");
             System.out.println("   |          _|_");
             System.out.println("   |         /   \\");
@@ -48,7 +44,6 @@ public class AnimationOfHangman {
             System.out.println("___|___");
         }
         if (lives == 2) {
-            System.out.println("Wrong guess, try again");
             System.out.println("   ____________");
             System.out.println("   |          _|_");
             System.out.println("   |         /   \\");
@@ -60,7 +55,6 @@ public class AnimationOfHangman {
             System.out.println("___|___");
         }
         if (lives == 1) {
-            System.out.println("Wrong guess, try again");
             System.out.println("   ____________");
             System.out.println("   |          _|_");
             System.out.println("   |         /   \\");
@@ -82,7 +76,6 @@ public class AnimationOfHangman {
             System.out.println("   |         / | \\");
             System.out.println("   |          / \\ ");
             System.out.println("___|___      /   \\");
-            System.out.println("\t\t\tGAME OVER!");
         }
     }
 }
