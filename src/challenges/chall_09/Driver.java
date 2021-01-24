cd220a5ef9b77f3cc084deac74584ebf8e3ae288
alan <salan2000@wp.pl>
2021-01-25 00:33:39 +0100

    Removed additional semicolon

--- src/challenges/chall_09/Driver.java
+++ src/challenges/chall_09/Driver.java
@@ -2,7 +2,7 @@ package challenges.chall_09;
 import java.util.Scanner;
 
 public class Driver {
-    private static Scanner input = new Scanner(System.in);;
+    private static Scanner input = new Scanner(System.in);
 
     private static String firstName(){
         System.out.println("First name to calculate: ");
