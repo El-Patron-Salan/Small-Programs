653a21d5f7bb5082353441781bfe7d636adaee64
alan <salan2000@wp.pl>
2021-01-25 00:32:56 +0100

    Added two methods where user makes input

--- src/challenges/chall_09/Driver.java
+++ src/challenges/chall_09/Driver.java
@@ -1,4 +1,16 @@
 package challenges.chall_09;
+import java.util.Scanner;
 
 public class Driver {
+    private static Scanner input = new Scanner(System.in);;
+
+    private static String firstName(){
+        System.out.println("First name to calculate: ");
+        return input.nextLine();
+    }
+    private static String secondName(){
+        System.out.println("Second name to calculate: ");
+        return input.nextLine();
+    }
+
 }
