cae71ffc25ebf4df7c9cc1ab30d88d5c9f1645cb
alan <salan2000@wp.pl>
2021-01-25 00:22:32 +0100

    Made main class which will implement methods from driver

---
+++ src/challenges/chall_09/LoveCalculator.java
@@ -0,0 +1,9 @@
+package challenges.chall_09;
+
+public class LoveCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("\t\tProvide two names to calculate their love");
+    }
+
+}
