21f5ffa6ec41ef762dc7c165f38f7f5d499bd065
alan <salan2000@wp.pl>
2021-01-27 00:59:18 +0100

    Added list for test purposes

--- src/challenges/chall_09/LoveCalculator.java
+++ src/challenges/chall_09/LoveCalculator.java
@@ -1,9 +1,17 @@
 package challenges.chall_09;
+import java.util.ArrayList;
 
 public class LoveCalculator {
 
     public static void main(String[] args) {
         System.out.println("\t\tProvide two names to calculate their love");
+        ArrayList<Integer> lol = new ArrayList<>();
+        lol.add(2);
+        lol.add(1);
+        lol.add(1);
+        lol.add(3);
+        lol.add(1);
+        Driver.calculateLoveChance(lol);
     }
 
 }
