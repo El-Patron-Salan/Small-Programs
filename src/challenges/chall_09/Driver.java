8699188435a376857f3b68975eb07b8d0b1672f0
alan <salan2000@wp.pl>
2021-02-10 02:53:00 +0100

    Now should be working

--- src/challenges/chall_09/Driver.java
+++ src/challenges/chall_09/Driver.java
@@ -60,7 +60,27 @@ public class Driver {
         }
     }
 
+    static int sumElements(int firstElement, int secondElement) {
+        return firstElement + secondElement;
+    }
+
+
     public static void calculateLoveChance(ArrayList<Integer> list) {
+        //while(!(list.size() <= 3)) { }
+
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for(int i = list.size(); !(i <= 3); i--) {
+            int midElement = 0;
+            for(int j = 0; j < list.size() / 2; j++) {
+                midElement = (list.size() / 2) -1;
+                tempList.add(sumElements(list.get(j), (list.get(list.size() - 1 - j))));
+            }
+            if(list.size() % 2 != 0) tempList.add(list.size() - 1, list.get(midElement));
+            //replace elements in list with tempList
+            list.clear();
+            list.addAll(tempList);
+            tempList.clear();
+        }
     }
 
 }
