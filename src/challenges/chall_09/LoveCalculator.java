b01dc48a317a921679b903caee8c38c9701699d7
alan <salan2000@wp.pl>
2021-02-10 13:00:05 +0100

    Made instance of calculator method from Driver class

--- src/challenges/chall_09/LoveCalculator.java
+++ src/challenges/chall_09/LoveCalculator.java
@@ -5,7 +5,8 @@ public class LoveCalculator {
 
     public static void main(String[] args) {
         System.out.println("\t\tProvide two names to calculate their love");
-
+        int chance = Driver.calculator();
+        System.out.println("Your fit love is " + chance + "%");
     }
 
 }
