ae67c22a433a230de373b56353281b764ca828ba
alan <salan2000@wp.pl>
2021-02-10 12:34:05 +0100

    Removed values for testing

--- src/challenges/chall_09/LoveCalculator.java
+++ src/challenges/chall_09/LoveCalculator.java
@@ -5,13 +5,7 @@ public class LoveCalculator {
 
     public static void main(String[] args) {
         System.out.println("\t\tProvide two names to calculate their love");
-        ArrayList<Integer> lol = new ArrayList<>();
-        lol.add(2);
-        lol.add(1);
-        lol.add(1);
-        lol.add(3);
-        lol.add(1);
-        Driver.show(lol);
+
     }
 
 }
