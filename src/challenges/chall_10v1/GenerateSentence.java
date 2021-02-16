91be84e9a390793646854597c719ea2c0a3ce74a
alan <salan2000@wp.pl>
2021-02-16 22:18:03 +0100

    Created main class

---
+++ src/challenges/chall_10v1/GenerateSentence.java
@@ -0,0 +1,7 @@
+package challenges.chall_10v1;
+
+public class GenerateSentence {
+    public static void main(String[] args) {
+        System.out.println("It's just a simple sentence generator with the same structure every time");
+    }
+}
