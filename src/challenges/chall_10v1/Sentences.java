1661e8f1a2d7ac2f0b14be4370c9519ba9e7b71a
alan <salan2000@wp.pl>
2021-02-17 13:26:09 +0100

    Made constructor with paths to files

--- src/challenges/chall_10v1/Sentences.java
+++ src/challenges/chall_10v1/Sentences.java
@@ -1,10 +1,25 @@
 package challenges.chall_10v1;
 
+import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Sentences {
-    final private static String theNode = "The";
+    final private String theNode;
+    final private String nounsPath;
+    final private String verbsPath;
+    final private String adjectivesPath;
+    final private String prepositionsPath;
+
     static int lengthOfSentence;
+    RandomWords randWords = new RandomWords();
+
+    Sentences() {
+        theNode = "The";
+        nounsPath = "nouns.txt";
+        verbsPath = "verbs.txt";
+        adjectivesPath = "adjectives.txt";
+        prepositionsPath = "preposition.txt";
+    }
 
     private static int randomLength() {
         return lengthOfSentence = ThreadLocalRandom.current().nextInt(7,8);
