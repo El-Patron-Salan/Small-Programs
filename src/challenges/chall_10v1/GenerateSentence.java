9c27763292f7d328a1d6946f2269f354aa9ff0fc
alan <salan2000@wp.pl>
2021-02-17 13:53:44 +0100

    Completed challenge

--- src/challenges/chall_10v1/GenerateSentence.java
+++ src/challenges/chall_10v1/GenerateSentence.java
@@ -1,7 +1,14 @@
 package challenges.chall_10v1;
 
+import java.io.FileNotFoundException;
+
 public class GenerateSentence {
     public static void main(String[] args) {
-        System.out.println("It's just a simple sentence generator with the same structure every time");
+        System.out.println("It's just a simple sentence generator");
+        try {
+            Sentences.generator();
+        }catch(FileNotFoundException e) {
+            System.out.println(e + " - file not found");
+        }
     }
 }
