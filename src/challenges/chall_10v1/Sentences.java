20a09024bc9dbd927565aa92208661e0cef885d7
alan <salan2000@wp.pl>
2021-02-17 11:38:17 +0100

    Added method to randomize the length of sentence

--- src/challenges/chall_10v1/Sentences.java
+++ src/challenges/chall_10v1/Sentences.java
@@ -1,7 +1,12 @@
 package challenges.chall_10v1;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Sentences {
-    final private static String firstNode = "The";
+    final private static String theNode = "The";
     static int lengthOfSentence;
 
+    private static int randomLength() {
+        return lengthOfSentence = ThreadLocalRandom.current().nextInt(7,8);
+    }
 }
