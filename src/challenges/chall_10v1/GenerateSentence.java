7daa935c4dbaefdab9a703eeba886caed2e4cf56
alan <salan2000@wp.pl>
2021-02-17 14:02:36 +0100

    Removed useless text

--- src/challenges/chall_10v1/GenerateSentence.java
+++ src/challenges/chall_10v1/GenerateSentence.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 
 public class GenerateSentence {
     public static void main(String[] args) {
-        System.out.println("It's just a simple sentence generator");
         try {
             Sentences.generator();
         }catch(FileNotFoundException e) {
