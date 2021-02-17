fdc52bb4a38927b3550f4129f4d985eaa17872cb
alan <salan2000@wp.pl>
2021-02-17 19:16:01 +0100

    New challenge - password generator -> already made arrays with symbols

---
+++ src/challenges/chall_11/PasswordGenerator.java
@@ -0,0 +1,23 @@
+package challenges.chall_11;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+    //arrays with optional symbols
+    private final char [] symbols = {'~', '`', '!', '@', '#' , '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+',
+            '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '"', ',', '<', '.', '>', '/', '?'};
+
+    private final char [] lowerCaseChars = {'a' , 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q' , 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' , 'z'};
+
+    private final char [] upperCaseChars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    private final int [] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Password length: ");
+    }
+}
