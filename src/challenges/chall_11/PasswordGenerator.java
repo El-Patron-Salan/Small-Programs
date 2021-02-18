d8215dd6946a23e3bda434aaf4368c0de329e5c2
alan <salan2000@wp.pl>
2021-02-18 08:27:35 +0100

    Implemented methods in main function

--- src/challenges/chall_11/PasswordGenerator.java
+++ src/challenges/chall_11/PasswordGenerator.java
@@ -3,7 +3,6 @@ package challenges.chall_11;
 import java.util.Scanner;
 
 public class PasswordGenerator {
-
     //arrays with optional symbols
     private final char [] symbols = {'~', '`', '!', '@', '#' , '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+',
             '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '"', ',', '<', '.', '>', '/', '?'};
@@ -16,8 +15,15 @@ public class PasswordGenerator {
 
     private final int [] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    //methods asking about external symbols
+    private static boolean wantsSymbols(char in, String kind, String example) {
+        System.out.print("Do you want " + kind + " in password like " + example + "?(Y/N) ");
+        return in == 'Y' || in == 'y';
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         //variables specifying the complexity of password
         boolean inclSymbols = false;
         boolean inclLowercaseChars = false;
@@ -34,5 +40,12 @@ public class PasswordGenerator {
         }
 
         //questions about symbols
+        inclSymbols = wantsSymbols(input.next().charAt(0), "symbols", "$,#,@ etc.");
+        //lowercaseCharacters
+        inclLowercaseChars = wantsSymbols(input.next().charAt(0), "lowercase characters", "a,b,c...");
+        //uppercaseCharacters
+        inclUppercaseChars = wantsSymbols(input.next().charAt(0), "uppercase characters", "A,B,C...");
+        //numbers
+        inclNumbers = wantsSymbols(input.next().charAt(0), "numbers", "0,1,2,3...");
     }
 }
