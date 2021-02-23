a363b92825f08fe23c3f4e8b1296e32d0f5715a5
alan <salan2000@wp.pl>
2021-02-23 06:11:19 +0100

    Changed variable type from int to char, since I did everything with chars in mind

--- src/challenges/chall_11/PasswordGenerator.java
+++ src/challenges/chall_11/PasswordGenerator.java
@@ -14,7 +14,7 @@ public class PasswordGenerator {
     private final char [] upperCaseChars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
             'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-    private final int [] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private final char [] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     //methods asking about external symbols
     private static boolean wantsSymbols(String kind, String example) {
