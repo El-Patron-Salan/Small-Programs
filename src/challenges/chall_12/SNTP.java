c73307828b6b12acd657528bbfcd945447b6d2b3
alan <salan2000@wp.pl>
2021-03-03 09:01:04 +0100

    Added method returning seconds with decimal point

--- src/challenges/chall_12/SNTP.java
+++ src/challenges/chall_12/SNTP.java
@@ -10,16 +10,22 @@ public class SNTP {
      * @param time  The object to take local time from
      * @return an hour in integer
      */
-    private int getHour(LocalDateTime time) {
+    private int hour(LocalDateTime time) {
         return time.getHour();
     }
     /**
      * Get minutes from object
      *
      * @param time  The object to take local time from
-     * @return the minutes in integer
+     * @return the minutes in integer accomplish
      */
-    private int getMinutes(LocalDateTime time) {
+    private int minutes(LocalDateTime time) {
         return time.getMinute();
+    }
+
+    private float seconds(LocalDateTime time) {
+        float seconds = time.getSecond();
+        float fraction = time.getNano() / 1_000000000f;
+        return seconds + fraction;
     }
 }
