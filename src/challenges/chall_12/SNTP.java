25221b83aed59ae5eeaea2d703755e7994696c17
alan <salan2000@wp.pl>
2021-03-03 06:18:51 +0100

    Added method that returns minutes in integer var

--- src/challenges/chall_12/SNTP.java
+++ src/challenges/chall_12/SNTP.java
@@ -7,10 +7,19 @@ public class SNTP {
     /**
      * Get hour from object
      *
-     * @param time  The object to take time from
+     * @param time  The object to take local time from
      * @return an hour in integer
      */
     private int getHour(LocalDateTime time) {
         return time.getHour();
+    }
+    /**
+     * Get minutes from object
+     *
+     * @param time  The object to take local time from
+     * @return the minutes in integer
+     */
+    private int getMinutes(LocalDateTime time) {
+        return time.getMinute();
     }
 }
