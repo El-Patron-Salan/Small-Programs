cd980229b58368a2e77b7d78786c4d24194294fa
alan <salan2000@wp.pl>
2021-03-03 06:11:24 +0100

    Started new challenge SNTP - added method that returns an hour in integer var

---
+++ src/challenges/chall_12/SNTP.java
@@ -0,0 +1,16 @@
+package challenges.chall_12;
+
+import java.time.LocalDateTime;
+
+public class SNTP {
+
+    /**
+     * Get hour from object
+     *
+     * @param time  The object to take time from
+     * @return an hour in integer
+     */
+    private int getHour(LocalDateTime time) {
+        return time.getHour();
+    }
+}
