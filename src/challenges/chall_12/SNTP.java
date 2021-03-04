b49c7e51cbbd6aa326bc9ed6d585a625f2d4dc65
alan <salan2000@wp.pl>
2021-03-04 07:20:45 +0100

    Initialized localTime

--- src/challenges/chall_12/SNTP.java
+++ src/challenges/chall_12/SNTP.java
@@ -1,11 +1,22 @@
 package challenges.chall_12;
 
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
 public class SNTP {
 
     public static void main(String[] args) {
+
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        OffsetDateTime utcDateTime = OffsetDateTime.now();
+
         //take specified values of local time
         LocalTimeClock local = new LocalTimeClock();
         //and UTC time
         ClockTimeUTC utc = new ClockTimeUTC();
+
+        System.out.println(local.showActualDate(localDateTime));
     }
 }
