52694fdd94a074aebf57b06e7995280a48454df2
alan <salan2000@wp.pl>
2021-03-04 06:56:44 +0100

    Made object of UTC time

--- src/challenges/chall_12/SNTP.java
+++ src/challenges/chall_12/SNTP.java
@@ -3,7 +3,9 @@ package challenges.chall_12;
 public class SNTP {
 
     public static void main(String[] args) {
-        //take specified values
+        //take specified values of local time
         LocalTimeClock local = new LocalTimeClock();
+        //and UTC time
+        ClockTimeUTC utc = new ClockTimeUTC();
     }
 }
