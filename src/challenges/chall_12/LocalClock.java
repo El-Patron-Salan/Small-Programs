e2bb5388de2187eeeb98eee83c28777b28b0fd02
alan <salan2000@wp.pl>
2021-03-04 06:18:27 +0100

    Added method to interface that prints actual date

--- src/challenges/chall_12/LocalClock.java
+++ src/challenges/chall_12/LocalClock.java
@@ -1,9 +1,11 @@
 package challenges.chall_12;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 interface LocalClock {
 
+    String showActualDate(LocalDateTime date);
     int localHours(LocalTime time);
     int localMinutes(LocalTime time);
     float localSeconds(LocalTime time);
