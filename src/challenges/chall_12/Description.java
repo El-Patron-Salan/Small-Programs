aa56ca3bdd597a16c2ae8e1d9dd99a8a663187b8
alan <salan2000@wp.pl>
2021-03-04 08:38:03 +0100

    Created class with description

---
+++ src/challenges/chall_12/Description.java
@@ -0,0 +1,10 @@
+package challenges.chall_12;
+
+class Description {
+
+    Description() {
+        System.out.println("\n\nDay of week -> Actual Date -> Actual hour:minute:second -> " +
+                "(How much add or reduce local time to match UTC time +/- HourMinute) -> " +
+                "Seconds behind or ahead local clock is to UTC +/- Seconds");
+    }
+}
