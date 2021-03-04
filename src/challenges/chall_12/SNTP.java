b1724da05228151b43c8a32405b9920533f44a19
alan <salan2000@wp.pl>
2021-03-04 07:58:15 +0100

    Added method to SNTP that compares UTC time and local time

--- src/challenges/chall_12/SNTP.java
+++ src/challenges/chall_12/SNTP.java
@@ -19,4 +19,21 @@ public class SNTP {
 
         System.out.println(local.showActualDate(localDateTime));
     }
+
+    private static void compareHoursMinutes(int localHours, int localMinutes,
+                                              int utcHours, int utcMinutes) {
+
+        int compareHours = utcHours - localHours;
+        int compareMinutes = utcMinutes - localMinutes;
+
+        if(compareHours > 0) {
+            System.out.printf("\n+%02d%02d", compareHours, compareMinutes);
+        }
+        else if(utcHours == localHours) {
+            System.out.println("0000");
+        }
+        else {
+            System.out.printf("\n%02d%02d", compareHours, compareMinutes);
+        }
+    }
 }
