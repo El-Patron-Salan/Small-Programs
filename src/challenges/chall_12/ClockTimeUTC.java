b8403eda44a29dd526b7d98d0492947628cc9116
alan <salan2000@wp.pl>
2021-03-04 05:40:27 +0100

    Added method that return an hour

--- src/challenges/chall_12/ClockTimeUTC.java
+++ src/challenges/chall_12/ClockTimeUTC.java
@@ -3,4 +3,15 @@ package challenges.chall_12;
 import java.time.ZonedDateTime;
 
 class ClockTimeUTC implements ClockUTC{
+
+    /**
+     * Get hour from ZonedDateTime
+     *
+     * @param time  The object to take UTC time
+     * @return an hour of UTC time
+     */
+    @Override
+    public int utcHours(ZonedDateTime time) {
+        return time.getHour();
+    }
 }
