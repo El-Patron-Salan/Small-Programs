7a48e27fd92d0bd652131ea687e268af33af40b3
alan <salan2000@wp.pl>
2021-03-09 11:00:19 +0100

    Created main class

---
+++ src/challenges/chall_14/MagicEightBall.java
@@ -0,0 +1,12 @@
+package challenges.chall_14;
+
+import java.util.Scanner;
+
+public class MagicEightBall {
+
+    private void entranceDesc() {
+        System.out.println("\tWelcome in Magic 8-Ball fortune teller!");
+        System.out.println("**Ask any question and see what awaits you**");
+        System.out.println("");
+    }
+}
