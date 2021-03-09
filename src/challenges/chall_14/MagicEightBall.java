6d6c2b45f843e964233cd3a489e9b06c30f10b21
alan <salan2000@wp.pl>
2021-03-09 11:01:35 +0100

    Added method that show an answer

--- src/challenges/chall_14/MagicEightBall.java
+++ src/challenges/chall_14/MagicEightBall.java
@@ -15,4 +15,13 @@ public class MagicEightBall {
         System.out.print("Your question: ");
         in.nextLine();
     }
+
+    private void simpleAnimationWithAnswer(String answer) throws InterruptedException {
+        String text = "Shaking...";
+        for(int i = 0; i < 3; ++i) {
+            Thread.sleep(1500);
+            System.out.printf("%s ", text);
+        }
+        System.out.println("\n\t\t\t" + answer);
+    }
 }
