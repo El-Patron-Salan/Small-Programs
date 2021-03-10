400589873753c309062004b377af4e721371f323
alan <salan2000@wp.pl>
2021-03-10 10:48:40 +0100

    Created main method

--- src/challenges/chall_15/CollatzConjecture.java
+++ src/challenges/chall_15/CollatzConjecture.java
@@ -1,5 +1,20 @@
 package challenges.chall_15;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class CollatzConjecture {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+
+            System.out.println("For what number you want make collatz conjecture?");
+            System.out.print("Number: ");
+            int x = sc.nextInt();
+
+        }catch(InputMismatchException e) {
+            System.out.println(e + " - wrong input");
+        }
+    }
 }
