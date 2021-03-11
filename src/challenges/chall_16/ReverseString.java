4261b6fab92faab4514ad3ed15951f56ba7ee103
alan <salan2000@wp.pl>
2021-03-11 13:47:09 +0100

    Made reversing with char array

--- src/challenges/chall_16/ReverseString.java
+++ src/challenges/chall_16/ReverseString.java
@@ -1,4 +1,16 @@
 package challenges.chall_16;
 
+import java.util.Scanner;
+
 public class ReverseString {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("String: ");
+        String toReverse = input.next();
+        char[] arr = toReverse.toCharArray();
+        for(int i = arr.length - 1; i >= 0; --i)
+            System.out.print(arr[i]);
+    }
+
 }
