50d4975940a6b43dcf76a735e02be9089444f2b8
alan <salan2000@wp.pl>
2021-03-31 13:12:32 +0200

    Added function to get surname

--- src/challenges/chall_17,6/ContactManagmentSystem.c
+++ src/challenges/chall_17,6/ContactManagmentSystem.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_NAME_LENGTH 16
+#define MAX_FNAME_LENGTH 16
+#define MAX_SNAME_LENGTH 32
 /*
  *This is simpler approach for making this program
  */
+char* inputFirstName();
+char* inputSurname();
 
 int main(int argC, char* argV[]) {
     
@@ -12,10 +15,19 @@ int main(int argC, char* argV[]) {
     return 0;
 }
 
-char* inputName() {
+char* inputFirstName() {
     
     char* name = malloc( sizeof(name) );
-    printf( "%s: ", "Name" );
-    fgets( name, MAX_NAME_LENGTH, stdin );
+    printf( "%s: ", "First name" );
+    fgets( name, MAX_FNAME_LENGTH, stdin );
     return name;
 }
+
+char* inputSurname() {
+        
+    char* sName = malloc( sizeof(sName) );
+    printf( "%s: ", "Surname: ");
+    fgets( sName, MAX_SNAME_LENGTH, stdin );
+    return sName;
+}
+
