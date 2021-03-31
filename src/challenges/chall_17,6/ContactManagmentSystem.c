7475a14ac75dbd27ca3efe16793fc52a5b846631
alan <salan2000@wp.pl>
2021-03-31 12:54:15 +0200

    Added function to get name

--- src/challenges/chall_17,6/ContactManagmentSystem.c
+++ src/challenges/chall_17,6/ContactManagmentSystem.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+#define MAX_NAME_LENGTH 16
+/*
+ *This is simpler approach for making this program
+ */
+
 int main(int argC, char* argV[]) {
-        
+    
 
     return 0;
+}
+
+char* inputName() {
+    
+    char* name = malloc( sizeof(name) );
+    printf( "%s: ", "Name" );
+    fgets( name, MAX_NAME_LENGTH, stdin );
+    return name;
 }
