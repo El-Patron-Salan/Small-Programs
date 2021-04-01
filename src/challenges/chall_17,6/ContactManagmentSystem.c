8dc27929abb916b6b3841ed98296794a74a1dae2
alan <salan2000@wp.pl>
2021-04-01 11:44:31 +0200

    Added new function and tested it

--- src/challenges/chall_17,6/ContactManagmentSystem.c
+++ src/challenges/chall_17,6/ContactManagmentSystem.c
@@ -3,16 +3,18 @@
 
 #define MAX_FNAME_LENGTH 16
 #define MAX_SNAME_LENGTH 32
-#define MAX_PHONENUMBER_LENGTH 14
+#define MAX_PHONENUMBER_LENGTH 15
 /*
  *This is simpler approach for making this program
  */
 char* inputFirstName();
 char* inputSurname();
+size_t inputPhoneNumber();
 
 int main(int argC, char* argV[]) {
     
-
+    size_t x = inputPhoneNumber();
+    printf( "\n%zu\n", x);
     return 0;
 }
 
@@ -27,13 +29,19 @@ char* inputFirstName() {
 char* inputSurname() {
         
     char* sName = malloc( sizeof(sName) );
-    printf( "%s: ", "Surname: ");
+    printf( "%s: ", "Surname");
     fgets( sName, MAX_SNAME_LENGTH, stdin );
     return sName;
 }
 
-int inputPhoneNumber() {
+
+//phone number functions
+size_t inputPhoneNumber() {
     
-    int phoneNumber;
+    char* phoneNumber = malloc( sizeof(phoneNumber) );
+    char* eptr;
+    printf( "%s: ", "Phone number" );
     
+    fgets( phoneNumber, MAX_PHONENUMBER_LENGTH, stdin );
+    return strtol(phoneNumber, &eptr, 10);
 }
